c917de95251c938c8dca2bb3e5bee3da9e411708
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-09 16:53:08 +0200

    Installs leaflet-maps

---
+++ src/Map.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import L from 'leaflet';
+
+class Map extends Component {
+
+
+  render() {
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+}
+
+export default Map;
