1ae4022e3eb7b0ecc529e5477fc280e23afb3ed2
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-13 20:39:29 +0200

    Map appears on the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Map'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+//import Map from './Map'
+
+const mapboxToken = 'pk.eyJ1Ijoiam93aXRhIiwiYSI6ImNqajYyY3BwaDBlZGwzcnBsMWp5bjRmZDIifQ.5k65J4sEZtVfYuRLAHqwmQ';
 
 class App extends Component {
+
+    state = {
+    lat: 52.229675,
+    lng: 21.012230,
+    zoom: 12,
+  } 
   render() {
+    const position = [this.state.lat, this.state.lng];
     return (
       <div className="App">
-        <Map />
+        <Map center={ position } zoom={ this.state.zoom }>
+          <TileLayer 
+          attribution='&copy; <a href="https://www.osm.org/">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={ position }>
+        </Marker>
+        </Map>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
-class Map extends Component {
 
+const mapboxToken = 'pk.eyJ1Ijoiam93aXRhIiwiYSI6ImNqajYyY3BwaDBlZGwzcnBsMWp5bjRmZDIifQ.5k65J4sEZtVfYuRLAHqwmQ';
+
+
+class Map extends Component {
+	 
 
   render() {
     return (
-      <div >
-        
+      <div>
+
+
       </div>
-    );
+    )
   }
 }
 
