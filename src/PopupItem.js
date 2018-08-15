bde8bc441c0f17258f05ec6d0ca9aaa96297294d
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-15 11:14:31 +0200

    Minor correction of wrong syntax to parks.js

---
+++ src/PopupItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Popup } from 'react-leaflet';
+
+class PopupItem extends Component {
+	 
+
+  render() {
+    return (
+      <div>
+
+        
+        	<Popup>
+        		Popup marker
+        	</Popup>
+        
+
+      </div>
+    )
+  }
+}
+
+export default PopupItem;
