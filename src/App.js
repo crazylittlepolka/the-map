9fe172c7ffd04f82d728f45e0efee5c69e0b234a
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-15 20:45:34 +0200

    New try with google maps no success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+
+
 import './App.css';
 import * as parks from './parks';
-import MapItem from './MapItem'
 
-const mapboxToken = 'pk.eyJ1Ijoiam93aXRhIiwiYSI6ImNqajYyY3BwaDBlZGwzcnBsMWp5bjRmZDIifQ.5k65J4sEZtVfYuRLAHqwmQ';
 
 class App extends Component {
 
@@ -12,17 +13,21 @@ class App extends Component {
     lng: 21.012230,
     zoom: 13,
   } 
+  const myMap = withScriptjs(withGoogleMap((props) => (<GoogleMap />)));
 
   render() {
+    
     const position = [this.state.lat, this.state.lng];
+
     return (
       <div className="App">
-
-        <MapItem
-          zoom={ this.state.zoom }
-          position={ position }
-        />
-
+        <GoogleMap
+          defaultZoom={14}
+          center={ { lat:  42.3601, lng: -71.0589 } }
+          >
+       
+      </GoogleMap>
+   
       </div>
     );
   }
