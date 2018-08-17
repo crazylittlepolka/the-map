fa97006d447ace13ad064e41bc8c38fe33fe4c12
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-17 19:03:00 +0200

    Minor changes to clean the code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {}
-    }
-    // binding this to event-handler functions
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    this.onMapClick = this.onMapClick.bind(this);
+class App extends Component {
+
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
   }
+
+    // binding this to event-handler functions
+    //this.onMarkerClick = this.onMarkerClick.bind(this);
+    //this.onMapClick = this.onMapClick.bind(this);
+  
   onMarkerClick = (props, marker, e) => {
     this.setState({
       selectedPlace: props,
@@ -32,39 +32,42 @@ class App extends React.Component {
   }
   render() {
     const style = {
-      width: '50vw',
-      height: '75vh',
-      'marginLeft': 'auto',
-      'marginRight': 'auto'
+      width: '70vw',
+      height: '100vh',
+
     }
     return (
-      <Map
-        item
-        xs = { 12 }
-        style = { style }
-        google = { this.props.google }
-        onClick = { this.onMapClick }
-        zoom = { 14 }
-        initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
-      >
-        <Marker
-          onClick = { this.onMarkerClick }
-          title = { 'Changing Colors Garage' }
-          position = {{ lat: 39.648209, lng: -75.711185 }}
-          name = { 'Changing Colors Garage' }
-        />
-        <InfoWindow
-          marker = { this.state.activeMarker }
-          visible = { this.state.showingInfoWindow }
-        >
+      <div>
 
-        </InfoWindow>
-      </Map>
+
+        <Map
+          item
+          xs = { 12 }
+          style = { style }
+          google = { this.props.google }
+          onClick = { this.onMapClick }
+          zoom = { 14 }
+          initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
+        >
+          <Marker
+            onClick = { this.onMarkerClick }
+            title = { 'Changing Colors Garage' }
+            position = {{ lat: 39.648209, lng: -75.711185 }}
+            name = { 'Changing Colors Garage' }
+          />
+          <InfoWindow
+            marker = { this.state.activeMarker }
+            visible = { this.state.showingInfoWindow }
+          >
+            <p>test</p>
+          </InfoWindow>
+        </Map>
+      </div>
     );
   }
 }
 export default GoogleApiWrapper({
-    api: (process.env.AIzaSyDX3Iq_WqGPYBaVmHvfMcydqRyUg1b2M6I)
+    apiKey: (process.env.AIzaSyDX3Iq_WqGPYBaVmHvfMcydqRyUg1b2M6I)
 })(App)
 
 
